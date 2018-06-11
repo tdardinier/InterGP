2dbe0709663720aa5e7a276b5ff762c60e1c82be
Thibault Dardinier <t.dardinier@gmail.com>
2018-06-11 16:14:32 -0700

    Working InterGP

--- InterGP/interGP.py
+++ InterGP/interGP.py
@@ -2,14 +2,84 @@ import numpy as np
 from scipy.optimize import fmin_tnc
 
 
+class InterGP:
+
+    def __init__(self, k, n=1):
+
+        self.k = k  # kernel function
+
+        self.X = None
+        self.Y = None
+        self.N = None
+
+    def generateMatrixCov(self, X1, X2):
+        m = []
+        for x1 in X1:
+            line = []
+            for x2 in X2:
+                line.append(self.k(x1, x2))
+            m.append(line)
+        return np.matrix(m)
+
+    def fit(self, X, Y):
+        # X = [X[0], ..., X[N-1]] -> dim n (array even if n == 1)
+        # Y = [Y[0], ..., Y[N-1]] -> scalars
+        assert (len(X) == len(Y)), "Wrong sizes X Y"
+        self.X = X
+        self.Y = Y
+        self.N = len(self.Y)
+        f = np.matrix(Y).reshape((self.N, 1))
+        K = self.generateMatrixCov(X, X)  # K(X, X)
+        self.B = np.linalg.inv(K)
+        self.A = self.B * f
+
+    def predictSingle(self, x):
+        K_star = self.generateMatrixCov([x], self.X)
+        k_double_star = self.k(x, x)
+        mean = K_star * self.A
+        var = k_double_star - K_star * self.B * (K_star.T)
+        return (mean, var)
+
+    def predictState(self, bounds, p=0.95):
+        # TODO p -> alpha
+        alpha = 1.96
+
+        def mu(x):
+            K_star = self.generateMatrixCov([x], self.X)
+            s = 0.0
+            for i in range(self.N):
+                s += K_star[0, i] * self.A[i, 0]
+            return s
+
+        def delta(x):
+            K_star = self.generateMatrixCov([x], self.X)
+            s = self.k(x, x)
+            for i in range(self.N):
+                for j in range(self.N):
+                    s -= K_star[0, i] * K_star[0, j] * self.B[i, j]
+            return s
+
+        def m(x):
+            return mu(x) - alpha * delta(x)
+
+        def M(x):
+            return mu(x) + alpha * delta(x)
+
+        def minusM(x):
+            return - M(x)
+
+        x0 = np.array([0.5 * (x[0] + x[1]) for x in bounds])
+        xa = fmin_tnc(m, x0, approx_grad=True, bounds=bounds)
+        xb = fmin_tnc(minusM, x0, approx_grad=True, bounds=bounds)
+
+        print(xa, xb)
+
+        return (m(xa[0]), M(xb[0]))
+
+
 def f(x):
-    s = 0.0
-    for xx in x:
-        s += (xx - 5) ** 2
-    return s
+    return x[0] * x[1]
 
-
-a = fmin_tnc(f, np.array([0, 0]), approx_grad=True, bounds=[(0, 8), (2, 8)])
 
 # x : n * 1, y : n * 1
 # exponential
@@ -22,82 +92,8 @@ def k(x, y):
     return np.exp(-0.5 * s)
 
 
-class InterGP:
+X = [[np.random.rand() * 10, np.random.rand() * 10] for i in range(100)]
+Y = [f(x) for x in X]
 
-    def __init__(self, k, ik, n=1):
-        self.k = k  # kernel function
-        self.ik = ik  # same function but on interval
-        self.n = n
-
-        self.X = None
-        self.Y = None
-
-    def generateMatrixCov(self, X1, X2, inter=False):
-        m = []
-        for x1 in X1:
-            line = []
-            for x2 in X2:
-                if inter:
-                    line.append(self.ik(x1, x2))
-                else:
-                    line.append(self.k(x1, x2))
-            m.append(line)
-        return np.matrix(m)
-
-    def fit(self, X, Y):
-        # X = [X[0], ..., X[N-1]] -> dim n (array even if n == 1)
-        # Y = [Y[0], ..., Y[N-1]] -> scalars
-        assert (len(X) == len(Y)), "Wrong sizes X Y"
-        self.X = X
-        self.Y = Y
-        N = len(self.Y)
-        f = np.matrix(Y).reshape((N, 1))
-        K = self.generateMatrixCov(X, X)  # K(X, X)
-        self.inv_K = np.linalg.inv(K)
-        self.inv_K_f = self.inv_K * f
-
-    def predict(self, x):
-
-        interX = [[In(yy) for yy in y] for y in self.X]
-
-        K_star = self.generateMatrixCov([x], interX, True)
-        print("K star", K_star)
-        k_double_star = self.ik(x, x)
-        print("K double star", k_double_star)
-
-        interInvK = InterMatrix.createFromMatrix(self.inv_K)
-        interInvKf = InterMatrix.createFromMatrix(self.inv_K_f)
-
-        print("interInvKf", interInvKf)
-
-        mean = InterMatrix.mult(K_star, interInvKf)
-
-        K_star_T = K_star.transpose()
-        prod = InterMatrix.mult(interInvK, K_star_T)
-        prod = InterMatrix.mult(K_star, prod)
-        prod = prod.neg()
-
-        var = In.add(k_double_star, prod.matrix[0][0])
-
-        # var = k_double_star - K_star * self.inv_K * (K_star.T)
-
-        return (mean, var)
-
-
-def f(x):
-    return x[0] * x[1]
-
-
-def test(n=50, noise=0.001, x=3, y=4):
-    X = [[np.random.rand() * 10, np.random.rand() * 10] for i in range(n)]
-    Y = [f(a) for a in X]
-
-    gp = InterGP(k, ik)
-    gp.fit(X, Y)
-    xx = [In(x), In(y)]
-    x = [In.fromNoise(x, noise), In.fromNoise(y, noise)]
-    a = gp.predict(xx)
-    b = gp.predict(x)
-    print("\nRESULTS:")
-    print(a)
-    print(b)
+gp = InterGP(k)
+gp.fit(X, Y)
